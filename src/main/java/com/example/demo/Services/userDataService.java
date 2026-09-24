6c8399fd6e4f506b246181044abdb5613ef2075b
Rehaan4u <rehaanmakhija@gmail.com>
2026-09-24 19:56:01 +0530

    Very important to include the @RestController to tell the compiler to consider it as the Controller

--- src/main/java/com/example/demo/Services/userDataService.java
+++ src/main/java/com/example/demo/Services/userDataService.java
@@ -3,11 +3,16 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.Models.userData;
 
 @Service 
+@RestController 
 public class userDataService {
+
+    @RequestMapping ("/products")
     public List<userData> returnUserList()
     {
        List<userData> userDataList = Arrays.asList(
